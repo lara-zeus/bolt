56bba68f200ba3074097f3a2de5e3922dd139a6d
Ash Monsh <wh7r.com@gmail.com>
2023-06-06 17:39:02 +0300

    Update Form.php

--- src/Models/Form.php
+++ src/Models/Form.php
@@ -5,9 +5,9 @@ namespace LaraZeus\Bolt\Models;
 use Database\Factories\FormFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasManyThrough;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\Translatable\HasTranslations;
 
