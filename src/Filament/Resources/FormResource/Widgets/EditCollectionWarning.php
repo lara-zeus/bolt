36944554a81a17b23079d0cbde1fc70fa9131f13
atmonshi <atmonshi@users.noreply.github.com>
2023-06-09 19:27:52 +0000

    Fix styling

--- src/Filament/Resources/FormResource/Widgets/EditCollectionWarning.php
+++ src/Filament/Resources/FormResource/Widgets/EditCollectionWarning.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class EditCollectionWarning extends Widget
 {
     protected int|string|array $columnSpan = 'full';
+
     public ?Model $record = null;
 
     protected static string $view = 'zeus-bolt::filament.resources.form-resource.widgets.edit-collection-warning';
