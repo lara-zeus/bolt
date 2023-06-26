aceaa5c8fff972f43488d79a28711596a11e8f71
Ash Monsh <wh7r.com@gmail.com>
2023-06-26 20:32:24 +0300

    convert text to phrases

--- resources/views/filament/resources/form-resource/widgets/edit-collection-warning.blade.php
+++ resources/views/filament/resources/form-resource/widgets/edit-collection-warning.blade.php
@@ -1,11 +1,11 @@
 <x-filament::widget>
     <x-filament::card>
         <div class="text-center font-semibold">
-            any changes in the values of these items will affect the responses for the forms in
+            {{ __('any changes in the values of these items will affect the responses for the forms in') }}
             <span class="text-primary-600">
                 {{ \LaraZeus\Bolt\Models\Field::whereJsonContains('options->dataSource', "$record->id")->count() }}
             </span>
-            field
+            {{ __('field') }}
         </div>
     </x-filament::card>
 </x-filament::widget>
