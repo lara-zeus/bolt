dcb82acd77c2cddb43d0c26d63c64e3c1c6fba91
Ash Monsh <wh7r.com@gmail.com>
2023-07-18 20:17:24 +0300

    wip

--- config/zeus-bolt.php
+++ config/zeus-bolt.php
@@ -26,21 +26,6 @@ return [
         'Section' => \LaraZeus\Bolt\Models\Section::class,
     ],
 
-    /**
-     * this will be setup the default seo site title. read more about it in 'laravel-seo'.
-     */
-    'site_title' => config('app.name', 'Laravel') . ' | Forms',
-
-    /**
-     * this will be setup the default seo site description. read more about it in 'laravel-seo'.
-     */
-    'site_description' => 'All about ' . config('app.name', 'Laravel') . ' Forms',
-
-    /**
-     * this will be setup the default seo site color theme. read more about it in 'laravel-seo'.
-     */
-    'site_color' => '#F5F5F4',
-
     'uploads' => [
         'disk' => 'public',
         'directory' => 'logos',
