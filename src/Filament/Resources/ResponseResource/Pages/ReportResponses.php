e10161f311f3b8300d124fc5a15320b1279777a9
atmonshi <atmonshi@users.noreply.github.com>
2023-07-23 14:33:47 +0000

    Fix styling

--- src/Filament/Resources/ResponseResource/Pages/ReportResponses.php
+++ src/Filament/Resources/ResponseResource/Pages/ReportResponses.php
@@ -2,8 +2,6 @@
 
 namespace LaraZeus\Bolt\Filament\Resources\ResponseResource\Pages;
 
-use AlperenErsoy\FilamentExport\Actions\FilamentExportBulkAction;
-use Closure;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
 use Filament\Resources\Pages\Page;
@@ -15,13 +13,11 @@ use Filament\Tables\Concerns\InteractsWithTable;
 use Filament\Tables\Contracts\HasTable;
 use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
-use Illuminate\Contracts\View\View;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use LaraZeus\Bolt\Filament\Resources\ResponseResource;
 use LaraZeus\Bolt\Models\Form;
 use LaraZeus\Bolt\Models\FormsStatus;
-use LaraZeus\Bolt\Models\Response;
 
 class ReportResponses extends Page implements HasForms, HasTable
 {
@@ -65,15 +61,15 @@ class ReportResponses extends Page implements HasForms, HasTable
         ];
 
         foreach ($this->form->fields->sortBy('ordering') as $field) {
-            $mainColumns[] = TextColumn::make('zeusData.'.$field->id)
+            $mainColumns[] = TextColumn::make('zeusData.' . $field->id)
                 ->label($field->name)
                 ->searchable(query: function (Builder $query, string $search): Builder {
                     return $query
                         ->whereHas('fieldsResponses', function ($query) use ($search) {
-                            $query->where('response', 'like', '%'.$search.'%');
+                            $query->where('response', 'like', '%' . $search . '%');
                         });
                 })
-                ->getStateUsing(fn(Model $record) => $this->getFieldResponseValue($record, $field))
+                ->getStateUsing(fn (Model $record) => $this->getFieldResponseValue($record, $field))
                 ->html()
                 ->toggleable();
         }
@@ -99,21 +95,21 @@ class ReportResponses extends Page implements HasForms, HasTable
                         ->icon('heroicon-o-eye')
                         ->visible($this->form_id !== 0)
                         ->label(__('Brows Entries'))
-                        ->url(fn(): string => ResponseResource::getUrl('brows').'?form_id='.request('form_id')),
+                        ->url(fn (): string => ResponseResource::getUrl('brows') . '?form_id=' . request('form_id')),
                     Action::make('list')
                         ->icon('heroicon-o-bars-4')
                         ->visible($this->form_id !== 0)
                         ->label(__('List Entries'))
-                        ->url(fn(): string => ResponseResource::getUrl().'?form_id='.$this->form_id),
+                        ->url(fn (): string => ResponseResource::getUrl() . '?form_id=' . $this->form_id),
                     Action::make('report')
                         ->icon('heroicon-o-document-chart-bar')
                         ->visible($this->form_id !== 0)
                         ->label(__('Entries Report'))
-                        ->url(fn(): string => ResponseResource::getUrl('report').'?form_id='.$this->form_id),
-                ])
+                        ->url(fn (): string => ResponseResource::getUrl('report') . '?form_id=' . $this->form_id),
+                ]),
             ])
             ->recordUrl(
-                fn(Model $record): string => ResponseResource::getUrl('view', ['record' => $record]),
+                fn (Model $record): string => ResponseResource::getUrl('view', ['record' => $record]),
             );
     }
 
