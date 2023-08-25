84d76b85caa5b75724c8f02a2758a044608843e7
Ash Monsh <wh7r.com@gmail.com>
2023-08-26 02:25:44 +0300

    Create Paragraph.php

---
+++ src/Fields/Classes/Paragraph.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace LaraZeus\Bolt\Fields\Classes;
+
+use Filament\Forms\Components\Placeholder;
+use LaraZeus\Bolt\Fields\FieldsContract;
+
+class Paragraph extends FieldsContract
+{
+    public string $renderClass = Placeholder::class;
+
+    public int $sort = 10;
+
+    public function title(): string
+    {
+        return __('Paragraph');
+    }
+}
