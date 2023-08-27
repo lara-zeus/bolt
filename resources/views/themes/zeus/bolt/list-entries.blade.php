e3578d62d7ee5b0ede582da90b8f45028c9c5d1e
Ash Monsh <wh7r.com@gmail.com>
2023-08-27 15:23:49 +0300

    remove duplicated breadcrumbs

--- resources/views/themes/zeus/bolt/list-entries.blade.php
+++ resources/views/themes/zeus/bolt/list-entries.blade.php
@@ -5,10 +5,6 @@
 
     <x-slot name="breadcrumbs">
         <li class="flex items-center">
-            <a href="{{ url('/') }}">{{ __('Home') }}</a>
-            @svg('iconpark-rightsmall-o','fill-current w-4 h-4 mx-3 rtl:rotate-180')
-        </li>
-        <li class="flex items-center">
             {{ __('browse your Entries') }}
         </li>
     </x-slot>
