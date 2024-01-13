33c12b1eb99c9413b35ea2a7799bf875ed868793
Ash Monsh <wh7r.com@gmail.com>
2024-01-13 21:48:39 +0300

    remove deprecated configration methods

--- src/Configuration.php
+++ src/Configuration.php
@@ -8,18 +8,6 @@ use LaraZeus\Bolt\Enums\Resources;
 trait Configuration
 {
     /**
-     * @deprecated deprecated since version 2.1
-     * set the default path for the forms homepage.
-     */
-    protected Closure | string $boltPrefix = 'bolt';
-
-    /**
-     * @deprecated deprecated since version 2.1
-     * the middleware you want to apply on all the forms routes
-     */
-    protected array $boltMiddleware = ['web'];
-
-    /**
      * you can overwrite any model and use your own
      */
     protected array $boltModels = [
@@ -36,79 +24,18 @@ trait Configuration
     protected array $hideResources = [];
 
     /**
-     * @deprecated deprecated since version 2.1
-     * default mailable for new entries
-     */
-    protected string $defaultMailable = \LaraZeus\Bolt\Mail\FormSubmission::class;
-
-    /**
      * available extensions, leave it null to disable the extensions tab from the forms
      */
     protected ?array $extensions = null;
-
-    /**
-     * where to upload all files when using the file upload field
-     *
-     * @deprecated deprecated since version 2.1
-     */
-    protected Closure | string $uploadDisk = 'public';
-
-    /**
-     * the directory name
-     *
-     * @deprecated deprecated since version 2.1
-     */
-    protected Closure | string $uploadDirectory = 'forms';
 
     /**
      * the resources navigation group
      */
     protected Closure | string $navigationGroupLabel = 'Bolt';
 
-    /*
-     * @deprecated deprecated since version 2.1
-     */
-    protected Closure | string | null $domain = null;
-
     protected Closure | bool $showNavigationBadges = true;
 
     protected array $showNavigationBadgesArray = [];
-
-    /*
-     * @deprecated deprecated since version 2.1
-     */
-    public function boltPrefix(Closure | string $prefix): static
-    {
-        $this->boltPrefix = $prefix;
-
-        return $this;
-    }
-
-    /*
-     * @deprecated deprecated since version 2.1
-     */
-    public function getBoltPrefix(): Closure | string
-    {
-        return $this->evaluate($this->boltPrefix);
-    }
-
-    /*
-     * @deprecated deprecated since version 2.1
-     */
-    public function boltMiddleware(array $middleware): static
-    {
-        $this->boltMiddleware = $middleware;
-
-        return $this;
-    }
-
-    /*
-     * @deprecated deprecated since version 2.1
-     */
-    public function getMiddleware(): array
-    {
-        return $this->boltMiddleware;
-    }
 
     public function boltModels(array $models): static
     {
@@ -130,42 +57,6 @@ trait Configuration
         )[$model];
     }
 
-    /*
-     * @deprecated deprecated since version 2.1
-     */
-    public function uploadDisk(Closure | string $disk): static
-    {
-        $this->uploadDisk = $disk;
-
-        return $this;
-    }
-
-    /*
-     * @deprecated deprecated since version 2.1
-     */
-    public function getUploadDisk(): Closure | string
-    {
-        return $this->evaluate($this->uploadDisk);
-    }
-
-    /*
-     * @deprecated deprecated since version 2.1
-     */
-    public function uploadDirectory(Closure | string $dir): static
-    {
-        $this->uploadDirectory = $dir;
-
-        return $this;
-    }
-
-    /*
-     * @deprecated deprecated since version 2.1
-     */
-    public function getUploadDirectory(): Closure | string
-    {
-        return $this->evaluate($this->uploadDirectory);
-    }
-
     public function navigationGroupLabel(Closure | string $label): static
     {
         $this->navigationGroupLabel = $label;
@@ -178,24 +69,6 @@ trait Configuration
         return $this->evaluate($this->navigationGroupLabel);
     }
 
-    /*
-     * @deprecated deprecated since version 2.1
-     */
-    public function defaultMailable(string $mailable): static
-    {
-        $this->defaultMailable = $mailable;
-
-        return $this;
-    }
-
-    /*
-     * @deprecated deprecated since version 2.1
-     */
-    public function getDefaultMailable(): string
-    {
-        return $this->defaultMailable;
-    }
-
     public function extensions(?array $extensions): static
     {
         $this->extensions = $extensions;
@@ -206,24 +79,6 @@ trait Configuration
     public function getExtensions(): ?array
     {
         return $this->extensions;
-    }
-
-    /*
-     * @deprecated deprecated since version 2.1
-     */
-    public function domain(Closure | string | null $domain): static
-    {
-        $this->domain = $domain;
-
-        return $this;
-    }
-
-    /*
-     * @deprecated deprecated since version 2.1
-     */
-    public function getDomain(): Closure | string | null
-    {
-        return $this->evaluate($this->domain);
     }
 
     public function hideResources(array $resources): static
