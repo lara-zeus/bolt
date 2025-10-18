97679d4daefc1210caea1f6d52639458d53e9b67
Ash Monsh <wh7r.com@gmail.com>
2025-10-18 14:13:23 +0300

    improve UX adding the field name and section to the field options modal

--- src/Concerns/Schema/Sections.php
+++ src/Concerns/Schema/Sections.php
@@ -78,7 +78,10 @@ trait Sections
                         ->tooltip('more field options')
                         ->icon('heroicon-m-cog')
                         ->modalIcon('heroicon-m-cog')
-                        ->modalDescription(__('zeus-bolt::forms.fields.settings'))
+                        ->modalHeading(__('zeus-bolt::forms.fields.options.label'))
+                        ->modalDescription(function(array $arguments, Repeater $component, Get $get){
+                            return $get('name') .' / '.$component->getState()[$arguments['item']]['name'] ?? '';
+                        })
                         ->fillForm(
                             fn (array $arguments, Repeater $component) => $component->getItemState($arguments['item'])
                         )
