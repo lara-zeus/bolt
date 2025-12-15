86351821d914bbc94f45903b88bf617d3c3f6522
Ash Monsh <wh7r.com@gmail.com>
2025-12-15 21:05:08 +0300

    wip

--- src/BoltServiceProvider.php
+++ src/BoltServiceProvider.php
@@ -35,9 +35,25 @@ class BoltServiceProvider extends PackageServiceProvider
     {
         CoreServiceProvider::setThemePath('bolt');
 
-        Livewire::component('bolt.fill-form', FillForms::class);
-        Livewire::component('bolt.list-forms', ListForms::class);
-        Livewire::component('bolt.list-entries', ListEntries::class);
+        Livewire::addLocation(
+            classNamespace: 'LaraZeus\\Bolt\\Livewire'
+        );
+        Livewire::addComponent(
+            name: 'bolt.fill-form',
+            class:FillForms::class
+        );
+        Livewire::addComponent(
+            name: 'bolt.list-forms',
+            class:ListForms::class
+        );
+        Livewire::addComponent(
+            name: 'bolt.list-entries',
+            class:ListEntries::class
+        );
+
+        //Livewire::component('bolt.fill-form', FillForms::class);
+        //Livewire::component('bolt.list-forms', ListForms::class);
+        //Livewire::component('bolt.list-entries', ListEntries::class);
     }
 
     /**
