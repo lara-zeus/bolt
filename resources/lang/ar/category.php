807535e97ec72c91117d1613fd022f3d808d77ae
Ash Monsh <wh7r.com@gmail.com>
2026-05-08 22:23:23 +0300

    add arabic translations AI generated

---
+++ resources/lang/ar/category.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'label' => 'التصنيف',
+    'plural_label' => 'التصنيفات',
+    'navigation_label' => 'التصنيفات',
+    'name' => 'الاسم',
+    'slug' => 'الرابط',
+    'ordering' => 'الترتيب',
+    'is_active' => 'مفعل',
+    'not_active' => 'غير مفعل',
+    'description' => 'الوصف',
+    'logo' => 'الشعار',
+    'forms' => 'النماذج',
+];
---
+++ resources/lang/ar/response.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'name' => 'الاسم',
+    'status' => 'الحالة',
+    'notes' => 'ملاحظات',
+    'created_at' => 'تاريخ الإنشاء',
+    'guest' => 'زائر',
+    'export_responses' => 'تصدير الاستجابات',
+    'entries_report' => 'تقرير الإدخالات',
+    'set_status' => 'تعيين الحالة',
+    'view_response_number' => 'عرض الاستجابة رقم ',
+    'view_the_entry' => 'عرض الإدخال',
+    'my_responses' => 'استجاباتي',
+    'show_entry' => 'عرض الإدخال',
+    'new_submission_in' => 'تسليم جديد في',
+    'avatar' => 'الصورة الرمزية',
+];
